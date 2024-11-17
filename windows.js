514649b7426da68d27ff1075ab0622d426be8d5d
chhatraman shrestha <cms111000111@gmail.com>
2024-11-17 14:24:34 +0545

    Add table search functionality to windows.js for improved data filtering

--- windows.js
+++ windows.js
@@ -624,3 +624,32 @@ function yes(msg = '') {
 	}
 	return prompt(msg) == 'yes';
 }
+
+var tablecount=0;
+    
+function initTableSearch(sid,id,data=[]){
+    tablecount+=1;
+    elements=document.querySelectorAll("#"+id +" tr");
+    console.log(elements);
+    console.log(data);
+    $("#"+sid).keyup(function(){
+        parameter=$('#'+sid).val().toLowerCase();
+        console.log(parameter);
+        elements=document.querySelectorAll("#"+id +" tr");
+        elements.forEach(element => {
+            done=false;
+            data.forEach(d => {
+                s_para=(""+$(element).data(d)+"").toLowerCase();
+                if(!done){
+                    if(s_para.includes(parameter)){
+                        $(element).show();
+                        done=true;
+                    }else{
+                        $(element).hide();
+                    }
+                }
+            });
+        });
+    });
+    
+}
