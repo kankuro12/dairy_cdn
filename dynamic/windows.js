79ba0d9bdd7b31216174a97488f38f60c15eb486
chhatraman shrestha <cms111000111@gmail.com>
2024-11-17 15:32:40 +0545

    Add new working section to dynamic window with title and loader image

--- dynamic/windows.js
+++ dynamic/windows.js
@@ -957,7 +957,17 @@ window.onload = function () {
 		</div>
 		<div class="content" id="wc"></div>
 		</div>
-	</div>`;
+	</div>
+	<div class="working" id="xxx_123">
+		<div class="working-child" >
+
+			<div class="w-100 working-title" id="yyy_123">
+				asdfasd
+			</div>
+			<img src="{{asset('backend/images/cude_loader.svg')}}" alt="">
+		</div>
+	</div>
+	`;
 
 	const placeholders = document.querySelector('#placeholders');
 	if (placeholders) {
